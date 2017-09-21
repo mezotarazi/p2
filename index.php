718a9e791fc967c7db55beb4a880a129d70b15b6
mezotarazi <mezotarazi@gmail.com>
2017-09-20 23:15:42 -0400

    V1 - all files.

---
+++ index.php
@@ -0,0 +1,202 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Resting Metabolic Rate (RMR)</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="container">
+
+    <div class="jumbotron">
+        <div>
+            <h2  >Resting Metabolic Rate (RMR) Calculator</h2></div>
+        <p>This Calculator is based on Mifflin St. Jeor Algorithm to calculate Calorie intake</p>
+
+
+
+    </br></br></br>
+        <ul class="nav nav-tabs" id="units-tabs" role="tablist">
+            <li role="presentation" class="active"><a href="#imperial" aria-controls="imperial" role="tab" data-toggle="tab">Imperial</a></li>
+            <li role="presentation"><a class="text-muted" href="#metric" aria-controls="metric" role="tab" data-toggle="tab">Metric</a></li>
+        </ul>
+
+	<div class="row">
+		<div class="col-sm-6 tab-content">
+
+						
+				<div role="tabpanel" class="tab-pane active" id="imperial">
+					<div id="form">
+
+			<form method="get" action="/results.php">
+			  
+			  <input type="hidden" name="measure" value="imperial">
+
+			  <table>
+			  
+			  <tr>
+			  <td class="col1">Gender</td>
+			  <td>
+			  <label><input type="radio" name="gender" id="male" value="male" checked> Male&nbsp;</label>
+			  <label><input type="radio" name="gender" id="female" value="female"> Female</label>
+			  </td>
+			  </tr>
+			  
+			  <tr>
+			  <td class="col1"><label for="age">Age</label></td>
+			  <td>
+			  <input type="text" name="age" class="form-control" id="age" style="width:60px;" maxlength="3" value="">
+			  </td>
+			  </tr>
+			  
+			  <tr>
+			  <td class="col1"><label for="weight">Weight</label></td>
+			  <td><input type="text" name="weight" class="form-control" id="weight" placeholder="lbs" style="width:60px;" maxlength="3" value=""></td>
+			  </tr>
+			  
+			  <tr>
+			  <td class="col1"><label for="height">Height</label></td>
+			  <td>
+			    <select name="height" class="form-control" id="height" style="width:100px;">
+			    <option value="55">4ft 7in</option>
+			    <option value="56">4ft 8in</option>
+			    <option value="57">4ft 9in</option>
+			    <option value="58">4ft 10in</option>
+			    <option value="59">4ft 11in</option>
+			    <option value="60">5ft 0in</option>
+			    <option value="61">5ft 1in</option>
+			    <option value="62">5ft 2in</option>
+			    <option value="63">5ft 3in</option>
+			    <option value="64">5ft 4in</option>
+			    <option value="65">5ft 5in</option>
+			    <option value="66">5ft 6in</option>
+			    <option value="67">5ft 7in</option>
+			    <option value="68">5ft 8in</option>
+			    <option value="69">5ft 9in</option>
+			    <option value="70" selected>5ft 10in</option>
+			    <option value="71">5ft 11in</option>
+			    <option value="72">6ft 0in</option>
+			    <option value="73">6ft 1in</option>
+			    <option value="74">6ft 2in</option>
+			    <option value="75">6ft 3in</option>
+			    <option value="76">6ft 4in</option>
+			    <option value="77">6ft 5in</option>
+			    <option value="78">6ft 6in</option>
+			    <option value="79">6ft 7in</option>
+			    <option value="80">6ft 8in</option>
+			    <option value="81">6ft 9in</option>
+			    <option value="82">6ft 10in</option>
+			    <option value="83">6ft 11in</option>
+			    <option value="84">7ft 0in</option>
+			    </select>
+			  </td>
+			  </tr>
+			  
+			  <tr>
+			  <td class="col1">Activity</td>
+			  <td>
+			    <select name="activity" class="form-control" style="width:200px;">
+			    <option value="1.2" selected>Inactive</option>
+			    <option value="1.375">Light Exercise (1-2 days/week)</option>
+			    <option value="1.55">Moderate Exercise (3-5 days/week)</option>
+			    <option value="1.725">Heavy Exercise (6-7 days/week)</option>
+			    <option value="1.9">Athlete (2x per day) </option>
+			    </select>
+			  </td>
+			  </tr>
+			  
+			  <tr>
+
+			  
+			  			
+			  <tr style="margin-top:15px;">
+			  <td class="col1">&nbsp;</td>
+			  <td><input type="submit" class="btn btn-submit btn-lg" id="submit" name="submit" value="Calculate!"></td>
+			  </tr>
+			  
+			  </table>
+			  
+			</form>
+		</div> <!-- end #form -->				</div>
+
+
+				<div role="tabpanel" class="tab-pane" id="metric">
+					<div id="form-metric">
+
+			<form name="form-metric-tagged" method="get" action="/results.php">
+
+			  <input type="hidden" name="measure" value="metric">
+
+			  <table>
+			  
+			  <tr>
+			  <td class="col1">Gender</td>
+			  <td>
+			  <label><input type="radio" name="gender" id="male-metric" value="male" checked> Male&nbsp;</label>
+			  <label><input type="radio" name="gender" id="female-metric" value="female"> Female</label>
+			  </td>
+			  </tr>
+			  
+			  <tr>
+			  <td class="col1"><label for="age">Age</label></td>
+			  <td>
+			  <input type="text" name="age" class="form-control" id="age-metric" style="width:60px;" maxlength="3" value="">
+			  </td>
+			  </tr>
+			  
+			  <tr>
+			  <td class="col1"><label for="weight">Weight</label></td>
+			  <td><input type="text" name="weight" class="form-control" id="weight-metric" placeholder="kg" style="width:60px;" maxlength="5"></td>
+			  </tr>
+			  
+			  <tr>
+			  <td class="col1"><label for="height-metric">Height</label></td>
+			  <td><input type="text" name="height" class="form-control" id="height-metric" placeholder="cm" style="width:60px;" maxlength="3"></td>
+			  </tr>
+			  
+			  <tr>
+			  <td class="col1">Activity</td>
+			  <td>
+			    <select name="activity" class="form-control" style="width:200px;">
+			    <option value="1.2" selected>Inactive (office job)</option>
+			    <option value="1.375">Light Exercise (1-2 days/week)</option>
+			    <option value="1.55">Moderate Exercise (3-5 days/week)</option>
+			    <option value="1.725">Heavy Exercise (6-7 days/week)</option>
+			    <option value="1.9">Athlete (2x per day) </option>
+			    </select>
+			  </td>
+			  </tr>
+			  
+			  <tr>
+
+			  			
+			  <tr style="margin-top:15px;">
+			  <td class="col1">&nbsp;</td>
+			  <td><input type="submit" class="btn btn-submit btn-lg" id="submit-metric" name="submit" value="Calculate!"></td>
+			  </tr>
+			  
+			  </table>
+			  
+			</form>
+		</div> <!-- end #form -->				</div>
+			
+					</div>
+
+
+	
+	<hr>
+</div>
+	
+	
+	</div> <!-- end .jumbotron -->
+
+</div> <!-- end .container -->
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ results.php
@@ -0,0 +1,144 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Resting Metabolic Rate(RMR) Results</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<?php
+include 'includes/functions.php';
+
+$calories = Mifflin($_GET['measure'],$_GET['gender'],$_GET['weight'],$_GET['height'],$_GET['age'],$_GET['activity']);
+
+$BMI = round(BMI($_GET['measure'],$_GET['weight'],$_GET['height']),2);
+
+$activity_array = array('1.0' => 'Minimal','1.2' => 'Inactive','1.376' => 'Light', '1.55' => 'Moderate','1.725' => 'Heavy', '1.9' => 'Athlete');
+?>
+
+<div class="container">
+
+    <div class="jumbotron">
+        <div class="result">
+            <div class="clearfix">
+		    <h1  class="pull-left">Results for Resting Metabolic Rate (RMR)</h1>
+
+		</div>
+
+	<p >You are a <strong><?php echo $_GET['age']?></strong> year old <strong><?php echo $_GET['gender']?></strong> who is <strong>
+            <?php echo $_GET['height'];
+            if ($_GET['measure']=='imperial'){
+                echo ' feet';
+            }else if ($_GET['measure']=='metric') {
+                echo ' cm';
+            }else echo $_GET['measure'];
+            ?></strong>
+        tall and weighs <strong><?php echo $_GET['weight'];
+            if ($_GET['measure']=='imperial'){
+                echo ' lb';
+            }else if ($_GET['measure']=='metric') {
+                echo ' kg';
+            }else echo $_GET['measure'];
+            ?>
+        </strong> while doing <strong><?php echo $activity_array[$_GET['activity']]?></strong>.</p>
+	
+	
+	 <hr class="style2"> 	
+
+		
+	<div class="row">
+		<div class="col-sm-4" style="padding-left:0;">	
+			<p class="bold text-center">Your Maintenance Calories</p>
+			<div id="tdee-cals">
+				<div style="padding-top:25px;">
+					<span class="h2"><?php echo $calories?></span>
+					<br>
+					<span class="cals">calories per day</span>
+				</div>
+				<hr>
+				<div>
+					<span class="h2"><?php echo $calories*7?></span>
+					<br>
+					<span class="cals">calories per week</span>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="col-sm-8" style="padding-top:60px;">
+
+			<table class="table table-condensed" id="bmr-table" style="margin-bottom:10px;">
+                <?php
+                foreach ($activity_array as $key => &$val){
+                    if ($calories == Mifflin($_GET['measure'],$_GET['gender'],$_GET['weight'],$_GET['height'],$_GET['age'], $key)) {
+                        echo '<tr class="success2"><td>' . $val . '</td>';
+                    }
+                    else echo '<tr><td>' . $val . '</td>';
+                    echo'<td>'.Mifflin($_GET['measure'],$_GET['gender'],$_GET['weight'],$_GET['height'],$_GET['age'], $key).'<small> calories per day </small></td></tr>';
+
+
+                }
+
+
+
+                ?>
+			</table>
+		</div> <!-- end col-sm-8 -->
+    </div>
+
+
+            </br>
+            </br>
+            </br>
+	
+	
+	<div class="row">
+		
+		<div class="col-sm-6" style="padding-left:0;">
+		
+			<h2>Ideal Weight:
+                <?php echo round(BMI_Assess($_GET['measure'],$_GET['height'],0),0);
+                echo " - ".round(BMI_Assess($_GET['measure'],$_GET['height'],1),0);
+                if ($_GET['measure']=='imperial'){
+                    echo ' lbs';
+                }else if ($_GET['measure']=='metric'){
+                    echo ' kg';
+                }
+                ?>
+
+            </h2>
+			<p>Your ideal body weight should be between:
+                <?php echo round(BMI_Assess($_GET['measure'],$_GET['height'],0),0);
+                echo " - ".round(BMI_Assess($_GET['measure'],$_GET['height'],1),0);
+                if ($_GET['measure']=='imperial'){
+                    echo ' lbs';
+                }else if ($_GET['measure']=='metric'){
+                    echo ' kg';
+                }
+                ?>
+            </p>
+        </div>
+		<div class="col-sm-6 " style="padding-right:0;">
+		
+			<h2>BMI Score: <?php echo $BMI ?></h2>
+		
+			<p>Your <strong>BMI</strong> is <strong><?php echo $BMI ?></strong>, which means you are classified as <strong>
+                    <?php echo
+                    BMI_Class($BMI);
+                    ?></strong>
+        </div>
+        </div> <!-- end .row -->
+		</div> <!-- end col-sm-6 -->
+	</div> <!-- end row -->
+	
+
+	
+</div> <!-- end .result -->
+
+
+</body>
+</html>
